09e5d15d3f6bdfafee2b62b818daa160e0630ccc
atenobi <79144475+atenobi@users.noreply.github.com>
2021-07-03 23:47:23 +0300

    lesson 10, unfinished slider

--- lesson-10/index.js
+++ lesson-10/index.js
@@ -49,46 +49,72 @@ const data = [
 	},
 ];
 
-const ulEl = document.querySelector('ul.content_item_container');
+// const ulEl = document.querySelector('ul.content_item_container');
+//
+// const dataExtract = function (arrEl, domEl) {
+// 	let img = document.createElement('img');
+// 	let name = document.createElement('p');
+// 	let location = document.createElement('p');
+//
+// 	 	img.className = 'content_image';
+// 		img.src = arrEl.imageUrl;
+//
+// 		name.innerHTML = `${arrEl.name}`;
+// 		name.classList.add('content_name_text');
+//
+// 		location.innerHTML = `${arrEl.city} ${arrEl.country}`;
+// 		location.classList.add('content_location_text');
+//
+// 		domEl.prepend(img)
+// 		img.after(name);
+// 		name.after(location);
+// 	return domEl;
+// }
+//
+// const outputAllEl = function (i) {
+// 	// let i = 0;
+// 	dataExtract(data[i], ulEl);
+// 	const cloneEl = function () {
+//
+// 		const cloner = (x) => x.cloneNode();
+// 		if (i < data.length) {
+// 			i++;
+// 			return ulEl.after(dataExtract(data[i], cloner(ulEl)));
+// 		} else {
+// 			i = 0;
+// 		}
+// 	}
+// 	if (i < data.length) {
+// 		cloneEl();
+// 		cloneEl();
+// 		cloneEl();
+// 	}
+// }
+//
+// outputAllEl(0);
 
-const dataExtract = function (arrEl, domEl) {
-	let img = document.createElement('img');
-	let name = document.createElement('p');
-	let location = document.createElement('p');
 
-	 	img.className = 'content_image';
-		img.src = arrEl.imageUrl;
+// var 2 (with unfinished changer)
+const output = document.querySelector('.container_content');
 
-		name.innerHTML = `${arrEl.name}`;
-		name.classList.add('content_name_text');
-
-		location.innerHTML = `${arrEl.city} ${arrEl.country}`;
-		location.classList.add('content_location_text');
-
-		domEl.prepend(img)
-		img.after(name);
-		name.after(location);
-	return domEl;
+function outputDataValues(arr) {
+	arr.forEach(element => {
+		let domEl = document.createElement('ul');
+		domEl.classList.add('content_item_container');
+			domEl.innerHTML = `<img src = '${element.imageUrl}' class="content_image">
+	    <p class="content_name_text">${element.name}</p>
+	    <p class="content_location_text">${element.city}</p> <p class="content_location_text">${element.country}</p>`
+		output.append(domEl);
+	})
 }
 
-const outputAllEl = function () {
-	let i = 0;
-	dataExtract(data[i], ulEl);
-	const cloneEl = function () {
-
-		const cloner = (x) => x.cloneNode();
-		if (i < data.length) {
-			i++;
-			return ulEl.after(dataExtract(data[i], cloner(ulEl)));
-		} else {
-			i = 0;
-		}
-	}
-	if (i < 4) {
-		cloneEl();
-		cloneEl();
-		cloneEl();
-	}
-}
-
-outputAllEl();
+// changer
+// outputDataValues(data.slice(0,4))
+//
+// const sliderListener = () => {
+// 	slideButtonEl.classList.add('pushed_button');
+// 	outputDataValues(data.slice(-4));
+// }
+//
+// const slideButtonEl = document.querySelector('.slide_button');
+// slideButtonEl.addEventListener('click', sliderListener);
