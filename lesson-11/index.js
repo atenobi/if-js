6aed69d2753d7f5a772936ca2f7e7206166d82a6
atenobi <79144475+atenobi@users.noreply.github.com>
2021-07-22 08:47:52 +0300

    lint+husky 2 var of lesson-11 homeworck

--- lesson-11/index.js
+++ lesson-11/index.js
@@ -1,60 +1,79 @@
-// // output element
-// const findElFunc = (x) => document.querySelector(x);
-// const adultsEl = findElFunc('p.form_adults_output');
-// const childrenEl = findElFunc('p.form_children_output');
-// const roomsEl = findElFunc('p.form_rooms_output');
-//
-// // // container
-// const personsEl = findElFunc('div.form_persons_container');
-//
-// const counterEl = findElFunc('div.persons_count_hidden');
-//
-// // elements
-// const adultsInputEl = findElFunc('input.adults_input');
-// const childrenInputEl = findElFunc('input.children_input');
-// const roomsInputEl = findElFunc('input.rooms_input');
-//
-// const changeCounter = function (x, y) {
-//   x.innerHTML = `${y.value}`;
-// };
-//
-// adultsInputEl.addEventListener('click', changeCounter(adultsEl, adultsInputEl));
-// childrenInputEl.addEventListener('click', changeCounter(childrenEl, childrenInputEl));
-// roomsInputEl.addEventListener('click', changeCounter(roomsEl, roomsInputEl));
-//
-// const counterVisible = () => {
-//   if (counterEl.classList.contains('persons_count_hidden')) {
-//     counterEl.classList.add('persons_count_visible');
-//     personsEl.classList.add('form_persons_clicked_container');
-//   }
-// };
-//
-// personsEl.addEventListener('click', counterVisible);
-//
-// const changeChildrenCounter = function (e) {
-//   let childrenAge;
-//   if (e.target.value > 0) {
-//     childrenAge = document.createElement('div');
-//     childrenAge.classList.add('child_age_visible');
-//     childrenAge.innerHTML = `<label for="child_age">
-//     What is the age of the child you’re travelling with?</label>
-//         <input type="number" min="0" max="17" value="0" id="child_age">`;
-//     counterEl.after(childrenAge);
-//   }
-//   if (e.target.value === 0) {
-//     childrenAge.classList.add('persons_count_hidden');
-//   }
-// };
-//
-// childrenInputEl.addEventListener('click', changeChildrenCounter);
+// full form text
+const formPersonsTextEl = document.querySelector('div.form_persons_container');
+// number value of counter in form
+const formAdultsEl = document.querySelector('span.form_adults');
+const formChildrenEl = document.querySelector('span.form_children');
+const formRoomsEl = document.querySelector('span.form_rooms');
+const counterEl = document.querySelector('div.persons_count');
 
-const countData = {
+// number value of counter in form
+const countAdultsEl = document.querySelector('span.count_adults');
+const countChildrenEl = document.querySelector('span.count_children');
+const countRoomsEl = document.querySelector('span.count_rooms');
+
+// container for values
+const personsValues = {
   adults: 0,
   children: 0,
   rooms: 0,
 };
 
-const result = {};
-dcdcS;
-SADASD;
-ASDASD;
+// counter +/- buttons
+const minesButtonEl = document.querySelectorAll('button.minus');
+const plusButtonEl = document.querySelectorAll('button.plus');
+
+// function for visibility change
+const changeVisibility = (el) => {
+  el.classList.toggle('visible_element');
+};
+
+// function for (-)
+const decreaseValue = (event) => {
+  if (event.target.nextElementSibling.classList.contains('count_adults')) {
+    if (personsValues.adults > 0) {
+      personsValues.adults -= 1;
+      formAdultsEl.innerHTML = personsValues.adults;
+      countAdultsEl.innerHTML = personsValues.adults;
+    }
+  } else if (event.target.nextElementSibling.classList.contains('count_children')) {
+    if (personsValues.children > 0) {
+      personsValues.children -= 1;
+      formChildrenEl.innerHTML = personsValues.children;
+      countChildrenEl.innerHTML = personsValues.children;
+    }
+  } else if (event.target.nextElementSibling.classList.contains('count_rooms')) {
+    if (personsValues.rooms > 0) {
+      personsValues.rooms -= 1;
+      formRoomsEl.innerHTML = personsValues.rooms;
+      countRoomsEl.innerHTML = personsValues.rooms;
+    }
+  }
+};
+
+// function for (+)
+const increaseValue = (event) => {
+  if (event.target.previousElementSibling.classList.contains('count_adults')) {
+    if (personsValues.adults < 30) {
+      personsValues.adults += 1;
+      formAdultsEl.innerHTML = personsValues.adults;
+      countAdultsEl.innerHTML = personsValues.adults;
+    }
+  } else if (event.target.previousElementSibling.classList.contains('count_children')) {
+    if (personsValues.children < 20) {
+      personsValues.children += 1;
+      formChildrenEl.innerHTML = personsValues.children;
+      countChildrenEl.innerHTML = personsValues.children;
+    }
+  } else if (event.target.previousElementSibling.classList.contains('count_rooms')) {
+    if (personsValues.rooms < 30) {
+      personsValues.rooms += 1;
+      formRoomsEl.innerHTML = personsValues.rooms;
+      countRoomsEl.innerHTML = personsValues.rooms;
+    }
+  }
+};
+
+// event listeners
+minesButtonEl.forEach((el) => el.addEventListener('click', decreaseValue));
+plusButtonEl.forEach((el) => el.addEventListener('click', increaseValue));
+formPersonsTextEl.addEventListener('click', () => changeVisibility(counterEl));
