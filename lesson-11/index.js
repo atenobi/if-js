98a14998d513f017d2862a42e9f52b94b1054090
atenobi <79144475+atenobi@users.noreply.github.com>
2021-07-23 02:39:16 +0300

    fix husky hooks, apdate lesson-11 homeworck var 2

--- lesson-11/index.js
+++ lesson-11/index.js
@@ -32,20 +32,20 @@ const decreaseValue = (event) => {
   if (event.target.nextElementSibling.classList.contains('count_adults')) {
     if (personsValues.adults > 0) {
       personsValues.adults -= 1;
-      formAdultsEl.innerHTML = personsValues.adults;
-      countAdultsEl.innerHTML = personsValues.adults;
+      formAdultsEl.textContent = personsValues.adults;
+      countAdultsEl.textContent = personsValues.adults;
     }
   } else if (event.target.nextElementSibling.classList.contains('count_children')) {
     if (personsValues.children > 0) {
       personsValues.children -= 1;
-      formChildrenEl.innerHTML = personsValues.children;
-      countChildrenEl.innerHTML = personsValues.children;
+      formChildrenEl.textContent = personsValues.children;
+      countChildrenEl.textContent = personsValues.children;
     }
   } else if (event.target.nextElementSibling.classList.contains('count_rooms')) {
     if (personsValues.rooms > 0) {
       personsValues.rooms -= 1;
-      formRoomsEl.innerHTML = personsValues.rooms;
-      countRoomsEl.innerHTML = personsValues.rooms;
+      formRoomsEl.textContent = personsValues.rooms;
+      countRoomsEl.textContent = personsValues.rooms;
     }
   }
 };
@@ -55,20 +55,20 @@ const increaseValue = (event) => {
   if (event.target.previousElementSibling.classList.contains('count_adults')) {
     if (personsValues.adults < 30) {
       personsValues.adults += 1;
-      formAdultsEl.innerHTML = personsValues.adults;
-      countAdultsEl.innerHTML = personsValues.adults;
+      formAdultsEl.textContent = personsValues.adults;
+      countAdultsEl.textContent = personsValues.adults;
     }
   } else if (event.target.previousElementSibling.classList.contains('count_children')) {
     if (personsValues.children < 20) {
       personsValues.children += 1;
-      formChildrenEl.innerHTML = personsValues.children;
-      countChildrenEl.innerHTML = personsValues.children;
+      formChildrenEl.textContent = personsValues.children;
+      countChildrenEl.textContent = personsValues.children;
     }
   } else if (event.target.previousElementSibling.classList.contains('count_rooms')) {
     if (personsValues.rooms < 30) {
       personsValues.rooms += 1;
-      formRoomsEl.innerHTML = personsValues.rooms;
-      countRoomsEl.innerHTML = personsValues.rooms;
+      formRoomsEl.textContent = personsValues.rooms;
+      countRoomsEl.textContent = personsValues.rooms;
     }
   }
 };
@@ -77,3 +77,42 @@ const increaseValue = (event) => {
 minesButtonEl.forEach((el) => el.addEventListener('click', decreaseValue));
 plusButtonEl.forEach((el) => el.addEventListener('click', increaseValue));
 formPersonsTextEl.addEventListener('click', () => changeVisibility(counterEl));
+
+// child age select element creation
+const childrenCountEl = document.querySelector('span.count_children');
+// container for children age
+const childAgeEl = document.querySelector('div.child_age');
+// text children age
+const childAgeTextEl = document.querySelector('p.child_age_text');
+// created age select element
+const selectEl = document.createElement('select');
+selectEl.classList.add('child_age_option_item');
+
+const makerAge = (fatherEl) => {
+  let optionEl = '';
+  let age = 1;
+  while (age < 18) {
+    optionEl = document.createElement('option');
+    optionEl.textContent = `${age} year old`;
+    age += 1;
+    fatherEl.appendChild(optionEl);
+  }
+  return fatherEl;
+};
+
+childAgeTextEl.after(makerAge(selectEl));
+
+const observer = new MutationObserver((mutationRecords) => {
+  const valueCount = mutationRecords[0].addedNodes[0].textContent;
+  if (valueCount > 0) {
+    childAgeEl.classList.add('visible_element');
+  } else {
+    childAgeEl.classList.remove('visible_element');
+  }
+});
+
+observer.observe(childrenCountEl, {
+  childList: true,
+  subtree: true,
+  characterDataOldValue: true,
+});
