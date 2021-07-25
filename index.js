8992ffc58055930ddcb6f4201dbab77e9ea4400e
atenobi <79144475+atenobi@users.noreply.github.com>
2021-07-26 02:43:35 +0300

    knowlege-check. need rewiew

--- index.js
+++ index.js
@@ -550,25 +550,266 @@
 // const students = new Students(studentsData);
 // console.log(students.getInfo());
 
-const textElLess9 = document.querySelectorAll('p.text_l_9');
+// const textElLess9 = document.querySelectorAll('p.text_l_9');
+//
+// const colors = {
+// 	data: ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'],
+// 	[Symbol.iterator] () { return this; },
+// 	next () {
+// 		let last = this.data.length;
+// 			if (this.current === undefined || this.current === last) {
+// 				this.current = 0;
+// 			}
+// 			if (this.current < last) {
+// 				return { done: false, value: this.data[this.current++], }
+// 			}
+// 	}
+// }
+//
+// const changeStyle = color => event => event.target.style.color = color.next().value;
+//
+// for (let i = 0; i < textElLess9.length; i++) {
+// 	textElLess9[i].addEventListener('click', changeStyle({...colors}));
+// }
 
-const colors = {
-	data: ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'],
-	[Symbol.iterator] () { return this; }, // ?
-	next () {
-		let last = this.data.length;
-			if (this.current === undefined || this.current === last) {
-				this.current = 0;
-			}
-			if (this.current < last) {
-				return { done: false, value: this.data[this.current++], }
-			}
-	}
+const roles = [
+  {
+    id: 'f7259abc-702a-4c26-99e4-ff596e7e3a4b',
+    name: 'admin'
+  },
+  {
+    id: '180e3ce7-ac3b-43a8-9c56-569a8c4493c8',
+    name: 'teacher'
+  },
+  {
+    id: '550a3276-c51f-465c-971f-ab4e34e9f02a',
+    name: 'student'
+  },
+];
+
+const users = [
+  {
+    id: '35172b2b-fb3d-4d8b-ac13-61351513dc5a',
+    userName: 'Nick',
+    email: 'nick@intellectfox.by',
+    role: 'teacher',
+    students: [
+      '2e6667e8-0352-48f0-8d6d-e38bf30338a5',
+      '649dc9a5-d788-49b6-bfc3-6716b97e66ad',
+    ],
+  },
+  {
+    id: '104acaf4-0255-4c02-bd71-9f68759a9d09',
+    userName: 'Andrew',
+    email: 'andrew@intellectfox.by',
+    role: 'Admin',
+  },
+  {
+    id: '9cf9e18f-2510-4011-90ef-ce690c4f7a66',
+    userName: 'Stas',
+    email: 'stas@intellectfox.by',
+    role: 'admin',
+  },
+  {
+    id: '3962c9f6-e152-4dbb-a6ac-6269879c942a',
+    userName: 'Mike',
+    email: 'mike@intellectfox.by',
+    role: 'Teacher',
+    students: [
+      'dc4e0a6d-fb2f-40c2-a57e-11cea1d82d47',
+      '0d337cf2-a172-4422-a4a1-716baf05cc0b',
+    ],
+  },
+  {
+    id: '6bed7b98-e40e-49ca-8826-fd1f8f0eaba6',
+    userName: 'Dan',
+    email: 'dan@intellectfox.by',
+    role: 'Teacher',
+    students: [
+      '292bda95-3018-4ca1-bf73-67737efe1b25',
+      '1730c5ac-e7ee-4f1a-b92a-2d59d23f78c2',
+    ],
+  },
+  {
+    id: '2e6667e8-0352-48f0-8d6d-e38bf30338a5',
+    userName: 'Charlie',
+    email: 'charlie@intellectfox.by',
+    role: 'student',
+    teachers: [
+      '35172b2b-fb3d-4d8b-ac13-61351513dc5a',
+      '2dfd5d40-0c21-4150-9b8c-58e52a146670',
+    ],
+  },
+  {
+    id: '649dc9a5-d788-49b6-bfc3-6716b97e66ad',
+    userName: 'Charlie',
+    email: 'charlie@intellectfox.by',
+    role: 'student',
+    teachers: [
+      '35172b2b-fb3d-4d8b-ac13-61351513dc5a',
+    ],
+  },
+  {
+    id: 'dc4e0a6d-fb2f-40c2-a57e-11cea1d82d47',
+    userName: 'Rebecca',
+    email: 'rebecca@intellectfox.by',
+    role: 'STUDENT',
+    teachers: [
+      '3962c9f6-e152-4dbb-a6ac-6269879c942a',
+      '2dfd5d40-0c21-4150-9b8c-58e52a146670',
+    ],
+  },
+  {
+    id: '0d337cf2-a172-4422-a4a1-716baf05cc0b',
+    userName: 'Jessica',
+    email: 'jessica@intellectfox.by',
+    role: 'Student',
+    teachers: [
+      '3962c9f6-e152-4dbb-a6ac-6269879c942a',
+    ],
+  },
+  {
+    id: '292bda95-3018-4ca1-bf73-67737efe1b25',
+    userName: 'Chris',
+    email: 'chris@intellectfox.by',
+    role: 'student',
+    teachers: [
+      '6bed7b98-e40e-49ca-8826-fd1f8f0eaba6',
+    ],
+  },
+  {
+    id: '1730c5ac-e7ee-4f1a-b92a-2d59d23f78c2',
+    userName: 'Kevin',
+    email: 'kevin@intellectfox.by',
+    role: 'student',
+    teachers: [
+      '6bed7b98-e40e-49ca-8826-fd1f8f0eaba6',
+      '2dfd5d40-0c21-4150-9b8c-58e52a146670',
+    ],
+  },
+  {
+    id: '2dfd5d40-0c21-4150-9b8c-58e52a146670',
+    userName: 'Deborah',
+    email: 'deborah@intellectfox.by',
+    role: 'TEACHER',
+    students: [
+      '2e6667e8-0352-48f0-8d6d-e38bf30338a5',
+      'dc4e0a6d-fb2f-40c2-a57e-11cea1d82d47',
+      '1730c5ac-e7ee-4f1a-b92a-2d59d23f78c2',
+    ],
+  },
+];
+
+// write migration for users array, role must be replaced to roleId from roles array.
+
+const apdateRole = () => {
+  let resultArr = [...users];
+  const isEqualStr = (x) => x.toString().toUpperCase();
+
+  users.forEach(userEl => {
+    roles.forEach(rolesEl => {
+      if (isEqualStr(userEl.role).includes(isEqualStr(rolesEl.name))) {
+        userEl.role = { id: rolesEl.id, name: rolesEl.name };
+        }
+      })
+    })
+  return resultArr;
+};
+
+console.log(apdateRole());
+
+// write iterator for each element users array, witch will return array [key, value],
+// uses only keys ['id', 'userName', 'email', 'students', 'roleId']. Important replace pole
+// roleId it would be replaced to role and contains object of roles array it dependence of roleId.
+
+// variant 1 with 'for in' circle
+
+// for (const entry in users[1]) {
+//   console.log([entry, users[1][entry]]);
+// }
+
+// variant 2 add Symbol iterator
+
+// const addIterator = function () {
+//   users.map((usersEL) => {
+//     usersEL[Symbol.iterator] = function () {
+//       let keys = Object.keys(this);
+//       let values = Object.values(this);
+//       let index = 0;
+//
+//       return {
+//         next: function () {
+//           return {
+//             value: [keys[index], values[index]],
+//             done: index++ >= keys.length
+//           };
+//         }
+//       }
+//     }
+//   })
+// };
+//
+// addIterator();
+//
+// for (const entry of users[1]) {
+//   console.log(entry);
+// }
+
+// create classes Admin, Teacher, Student
+
+// admin
+
+class Person {
+  constructor(arrayItem) {
+    this.id = arrayItem.id;
+    this.userName = arrayItem.userName;
+    this.email = arrayItem.email;
+    this.role = arrayItem.role;
+    // this.name = Array.role.name;
+  }
 }
 
-const changeStyle = color => event => event.target.style.color = color.next().value;
-
-for (let i = 0; i < textElLess9.length; i++) {
-	textElLess9[i].addEventListener('click', changeStyle({...colors}));
+class Admin extends Person {
+  constructor(arrayItem) {
+   super(arrayItem);
+  }
 }
 
+const admin = new Admin(users[2]);
+console.log(admin);
+
+// Teacher
+
+class Teacher extends Person {
+  constructor(arrayItem) {
+    super(arrayItem);
+   this.studentIds = [arrayItem.students];
+  }
+  getStudents() {
+
+  }
+}
+
+const teacher = new Teacher(users[3]);
+console.log(teacher);
+
+console.log(teacher.getStudents());
+// [Student {...}, Student {...}]
+
+// Student
+
+class Student extends Person {
+  constructor(arrayItem) {
+    super(arrayItem);
+    this.teacherIds = [arrayItem.teachers];
+  }
+  getTeachers() {
+
+  }
+}
+
+const student = new Student(users[5]);
+console.log(student);
+
+console.log(student.getTeachers());
+// [Teacher {...}, Teacher {...}]
