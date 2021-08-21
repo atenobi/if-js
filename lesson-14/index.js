857f064f25c9863cef6ad7bd6e34a31b321b60e6
atenobi <79144475+atenobi@users.noreply.github.com>
2021-08-21 19:13:34 +0300

    fixed variant 14/14a homeworck

--- lesson-14/index.js
+++ lesson-14/index.js
@@ -1,24 +1,15 @@
 const formDataEl = document.querySelector('form.file-form');
 
-const chooseImageDivEl = document.querySelector('div.choose_image_div');
-
-const getUrlImage = (file) => new Promise((resolve) => {
-  const reader = new FileReader();
-  reader.addEventListener('load', (event) => {
-    resolve(event.target.result);
-  });
-  reader.readAsDataURL(file);
-});
-
-formDataEl.addEventListener('submit', async (event) => {
+const sendData = async (event) => {
   event.preventDefault();
-  const dataFile = new FormData(formDataEl);
+  const fetchFile = {
+    method: 'POST',
+    body: new FormData(formDataEl),
+  };
+  const result = await fetch('https://fe-student-api.herokuapp.com/api/file', fetchFile)
+    .then((response) => response.json())
+    .catch(() => console.log(new Error('Sorry something wrong!')));
+  console.log(result);
+};
 
-  const file = dataFile.get('file');
-  const imageUrl = await getUrlImage(file);
-
-  const image = document.createElement('img');
-  image.setAttribute('src', `${imageUrl}`);
-  image.setAttribute('class', 'div_image');
-  chooseImageDivEl.prepend(image);
-});
+formDataEl.addEventListener('submit', sendData);
