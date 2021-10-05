6badfd59304012b1d4bb3809bcd24a6a2ce664e6
atenobi <79144475+atenobi@users.noreply.github.com>
2021-10-06 01:17:48 +0300

    cosmetic fixes: url/ formData/ object

--- lesson-15/index.js
+++ lesson-15/index.js
@@ -1,8 +1,9 @@
-const hotelsApi = 'https://fe-student-api.herokuapp.com/api/hotels';
+const baseUrl = 'https://fe-student-api.herokuapp.com/api/';
+const hotelsApi = `${baseUrl}hotels`;
 
 // page elements
 const outputDivEl = document.querySelector('div.js-homes-guests-content');
-const destinationButtonEl = document.querySelector('input.js_search_button');
+const destinationButtonEl = document.querySelector('input.js-search-button');
 const availableOutputContentEl = document.querySelector('div.js-output-hotels');
 const topSectionInputEl = document.querySelector('input#destination');
 const availableHotelsDivEl = document.querySelector('div.js-available-hotels');
@@ -25,6 +26,7 @@ function bubbleSort(arr) {
 
 // visualization in page result block
 function outputDataValues(arr, output) {
+  availableOutputContentEl.innerHTML = '';
   arr.forEach((element) => {
     const domEl = document.createElement('ul');
     domEl.classList.add('content_item_container');
@@ -53,7 +55,7 @@ if (storageData.length === 0) {
   outputDataValues(JSON.parse(storageData.hotels).slice(0, 4), outputDivEl);
 }
 
-// searching function
+// searching function old version not correct searching
 const userSearch = (event) => {
   event.preventDefault();
   const userSearchString = topSectionInputEl.value.toLowerCase();
@@ -74,7 +76,6 @@ const userSearch = (event) => {
 };
 
 destinationButtonEl.addEventListener('click', userSearch);
-// lesson-11
 
 // full form text
 const formPersonsTextEl = document.getElementById('js-form-persons-text');
@@ -87,17 +88,32 @@ const counterEl = document.querySelector('div.persons_count');
 
 // container for values
 const personsValues = {
-  adults: 0,
-  children: 0,
-  childrenAge: [],
-  rooms: 0,
+  adults: {
+    min: 0,
+    max: 30,
+    default: 1,
+    value: 0,
+  },
+  children: {
+    min: 0,
+    max: 10,
+    default: 0,
+    value: 0,
+    childrenAge: [],
+  },
+  rooms: {
+    min: 0,
+    max: 30,
+    default: 1,
+    value: 0,
+  },
 };
 
 // assignment value from object to text elements on form and counter
 const assignmentFunc = () => {
-  formAdultsEl.forEach((el) => el.textContent = personsValues.adults);
-  formChildrenEl.forEach((el) => el.textContent = personsValues.children);
-  formRoomsEl.forEach((el) => el.textContent = personsValues.rooms);
+  formAdultsEl.forEach((el) => el.textContent = personsValues.adults.value);
+  formChildrenEl.forEach((el) => el.textContent = personsValues.children.value);
+  formRoomsEl.forEach((el) => el.textContent = personsValues.rooms.value);
 };
 
 // counter +/- buttons
@@ -113,12 +129,17 @@ const changeVisibility = (el) => {
 const childAgeEl = document.querySelector('div.child_age');
 const childSelectsWrapperEl = document.querySelector('div.child_age_selects');
 const childSelectOriginEl = document.getElementById('js-childAge');
-// const childSelectAllEl = document.querySelectorAll('select.js-age-array');
-// childSelectAllEl.forEach((el) => personsValues.childrenAge.push(el.value));
+const childSelectAllEl = document.querySelectorAll('select.js-age-array');
+
+const getAge = () => {
+  childSelectAllEl.forEach((el) => personsValues.children.childrenAge.push(el.value));
+};
+
+getAge();
 
 // making select clones
 const creationSelectElements = () => {
-  if (personsValues.children > 1) {
+  if (personsValues.children.value > 1) {
     const clone = childSelectOriginEl.cloneNode(true);
     clone.classList.add('js-age-array');
     childSelectsWrapperEl.appendChild(clone);
@@ -127,17 +148,17 @@ const creationSelectElements = () => {
 
 // children age change functions
 const childIncrease = () => {
-  if (childSelectsWrapperEl.childNodes.length < 10) {
+  if (personsValues.children.value < personsValues.children.max) {
     creationSelectElements();
     childAgeEl.classList.add('visible_element');
   }
 };
 
 const childDecrease = () => {
-  if (childSelectsWrapperEl.childNodes.length > 0) {
+  if (personsValues.children.value > personsValues.children.min) {
     childSelectsWrapperEl.removeChild(childSelectsWrapperEl.lastChild);
   }
-  if (childSelectsWrapperEl.childNodes.length === 0) {
+  if (personsValues.children.value === personsValues.children.default) {
     childAgeEl.classList.remove('visible_element');
   }
 };
@@ -145,17 +166,17 @@ const childDecrease = () => {
 // function for (-)
 const decreaseValue = (event) => {
   if (event.target.nextElementSibling.classList.contains('count_adults')) {
-    if (personsValues.adults > 0) {
-      personsValues.adults -= 1;
+    if (personsValues.adults.value > personsValues.adults.min) {
+      personsValues.adults.value -= 1;
     }
   } else if (event.target.nextElementSibling.classList.contains('count_children')) {
-    if (personsValues.children > 0) {
-      personsValues.children -= 1;
+    if (personsValues.children.value > personsValues.children.min) {
+      personsValues.children.value -= 1;
       childDecrease();
     }
   } else if (event.target.nextElementSibling.classList.contains('count_rooms')) {
-    if (personsValues.rooms > 0) {
-      personsValues.rooms -= 1;
+    if (personsValues.rooms.value > personsValues.rooms.min) {
+      personsValues.rooms.value -= 1;
     }
   }
   assignmentFunc();
@@ -164,17 +185,17 @@ const decreaseValue = (event) => {
 // function for (+)
 const increaseValue = (event) => {
   if (event.target.previousElementSibling.classList.contains('count_adults')) {
-    if (personsValues.adults < 30) {
-      personsValues.adults += 1;
+    if (personsValues.adults.value < personsValues.adults.max) {
+      personsValues.adults.value += 1;
     }
   } else if (event.target.previousElementSibling.classList.contains('count_children')) {
-    if (personsValues.children < 10) {
-      personsValues.children += 1;
+    if (personsValues.children.value < personsValues.children.max) {
+      personsValues.children.value += 1;
       childIncrease();
     }
   } else if (event.target.previousElementSibling.classList.contains('count_rooms')) {
-    if (personsValues.rooms < 30) {
-      personsValues.rooms += 1;
+    if (personsValues.rooms.value < personsValues.rooms.max) {
+      personsValues.rooms.value += 1;
     }
   }
   assignmentFunc();
@@ -183,32 +204,33 @@ const increaseValue = (event) => {
 // event listeners
 minesButtonEl.forEach((el) => el.addEventListener('click', decreaseValue));
 plusButtonEl.forEach((el) => el.addEventListener('click', increaseValue));
+
 formPersonsTextEl.addEventListener('click', () => {
   formPersonsTextEl.classList.toggle('form_persons_clicked_container');
 });
 formPersonsTextEl.addEventListener('click', () => changeVisibility(counterEl));
 
-const formPersonsValue = new FormData(formPersonsTextEl);
+// make form data
+const formPersonsValue = new FormData();
 
 const saveUserFormPoles = () => {
-  formPersonsValue.set('adults', `${personsValues.adults}`);
-  formPersonsValue.set('children', `${personsValues.children}`);
-  formPersonsValue.set('rooms', `${personsValues.rooms}`);
+  formPersonsValue.set('adults', `${personsValues.adults.value}`);
+  formPersonsValue.set('children', `${personsValues.children.value}`);
+  formPersonsValue.set('rooms', `${personsValues.rooms.value}`);
 };
 
 // final compile of url and show it in console
-const showResult = (event) => {
+const showResult = async (event) => {
   event.preventDefault();
-  saveUserFormPoles();
+  await saveUserFormPoles();
+  // need clean up
   const search = `?search=${topSectionInputEl.value}`;
-  const url = new URL(hotelsApi).toString();
   const adults = `&adults=${formPersonsValue.get('adults')}`;
   const children = `&children=${formPersonsValue.get('children')}`;
   const rooms = `&rooms=${formPersonsValue.get('rooms')}`;
-  const resultStr = `${url}${search}${adults}${children}${rooms}`;
-  console.log(resultStr);
 
-  return resultStr;
+  const resultStr = `${hotelsApi}${search}${adults}${children}${rooms}`;
+  console.log(resultStr);
   // console.log(personsValues.childrenAge);
 };
 
